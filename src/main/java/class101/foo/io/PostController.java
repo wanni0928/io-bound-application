e4e14071be3cd5676fa73f6e11b5205fc7f7a7fe
wanni0928 <chlgudwnd123@gmail.com>
2021-03-04 23:26:10 +0900

    feat: add paging

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -1,6 +1,9 @@
 package class101.foo.io;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,19 +14,21 @@ public class PostController {
     @Autowired
     PostRepository postRepository;
 
+    private static Integer PAGE_SIZE = 20;
+
     // 1. 글을 작성한다.
     @PostMapping("/post")
     public Post createPost(@RequestBody Post post) {
         return postRepository.save(post);
     }
 
-    // 2-1. 글 목록을 조회한다.
+    // 2. 글 목록을 조회한다.
     @GetMapping("/posts")
-    public List<Post> getPostList() {
-        return postRepository.findAll();
+    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
+        return postRepository.findAll(
+                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+        );
     }
-    
-    // 2-2 글 목록을 페이징하여 반환
     
     // 3. 글 번호로 조회
     
